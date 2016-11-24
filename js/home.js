43552c546df2d30135cceeb51463fe45ae6e2bf6
Abby Moore <abbyleighanne@gmail.com>
2016-11-24 12:12:04 -0600

    v4 part 1

--- js/home.js
+++ js/home.js
@@ -1,32 +1,57 @@
 console.log("home.js linked");
 
-
+/* ----- DOM ELEMENTS ----- */
 var homeLink = document.getElementById("link-home");
 var homeView = document.getElementById("home-view");
 var output = document.getElementById("songListDom");
 
-var songs = [];
+
+/* ----- XHR REQUEST TO READ JSON FILE ----- */
+
+var songLoader = new XMLHttpRequest();
+songLoader.open("GET", "data/songs.json");
+songLoader.send();
+
+ songLoader.addEventListener("load", function () {
+  songs = JSON.parse(this.responseText).songs;
+  populatePage(songs);
+});
 
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+ /* ----- POPULATE PAGE ----- */
+
+ function populatePage(songs) {
+  
+  for (var i = 0; i < songs.length; i++) {
+
+  	var getSongs = songs[i];
+
+  	output.innerHTML += 
+  	`<div id="song${i}">
+  		${getSongs.title} by ${getSongs.artist} on the album ${getSongs.album}
+  		<button id="delete${i}">X</button>
+  	</div>`;
+  }
+
+  addListener(songs)
+  //console.log(songs);
+
+}
+
+function addListener(songs){
+	for (var i = 0; i < songs.length; i++) {
+
+		var deletebtn = document.getElementById(`delete${i}`);
+		deletebtn.addEventListener("click", deleteSong);
+		//console.log(deletebtn);
+	}
+}
 
 
-//add item to beginning of array
-songs.unshift("Light > by John Bryant on the album Twenty Something");
-
-//add item to end of array
-songs.push("Make It Holy > by The Staves on the album If I Was");
-
-//Replace > with -
-//add to the DOM
-
-for (i = 0; i < songs.length; i++) {
-	var replacedSongs = songs[i].replace(/>/g, "-").replace(/[@(!*]/g, "");
-	output.innerHTML += "<div>" + replacedSongs + "</div>";
+function deleteSong(event){
+	var targetSong = event.target.parentElement;
+	targetSong.parentNode.removeChild(targetSong);
 }
 
 
